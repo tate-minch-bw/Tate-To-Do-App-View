3d8f1b555ce44a58e67243d7e249bde9acd3a2a8
tminch <tminch@bandwidth.com>
2023-05-30 09:21:03 -0600

    fixing indentation, not sure where that got messed up to begin with

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 interface propType {
-    id:number;
-    task:string;
+  id:number;
+  task:string;
 }
 
 export default function TaskCard(props:propType){
-    return (
-        <div className="p-2 flex">
-          <div className="relative bg-white rounded-lg p-4 text-center w-96 mx-auto">
-            <div className="text-black mb-2">{props.task}</div>
-            <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
-              Edit
-            </button>
-            <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
-              Done
-            </button>
-          </div>
+  return (
+      <div className="p-2 flex">
+        <div className="relative bg-white rounded-lg p-4 text-center w-96 mx-auto">
+          <div className="text-black mb-2">{props.task}</div>
+          <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
+            Edit
+          </button>
+          <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
+            Done
+          </button>
         </div>
-      );
+      </div>
+    );
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -8,25 +8,25 @@ interface propType {
 }
 
 export default function UserCard(props: propType) {
-    const clickHandler = () => {
-      props.setUserId(props.id);
-    };
+  const clickHandler = () => {
+    props.setUserId(props.id);
+  };
 
-    return (
-      <div className="p-2 flex">
-      <div onClick={clickHandler} className="bg-white rounded-lg p-4 text-center cursor-pointer w-44 mx-auto">
-        <div className="text-black">{props.name}</div>
-      </div>
-      <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
-        ||
-      </button>
-      <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
-        x
-      </button>
+  return (
+    <div className="p-2 flex">
+    <div onClick={clickHandler} className="bg-white rounded-lg p-4 text-center cursor-pointer w-44 mx-auto">
+      <div className="text-black">{props.name}</div>
     </div>
+    <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
+      ||
+    </button>
+    <button className="bg-blue-500 text-white rounded-lg p-2 text-sm ml-2">
+      x
+    </button>
+  </div>
 
-    );
-  }
+  );
+}
 
 
 
